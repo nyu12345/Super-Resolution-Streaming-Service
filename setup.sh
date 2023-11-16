140a973906951398615074ad81b924424fb68bcd
nyu12345 <nathan.yu@duke.edu>
2023-11-16 12:05:21 -0500

    test python bash call

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-pip install --upgrade pip
 pip install -r requirements.txt
 
 git submodule update --init --recursive
 cd real-esrgan
-pip install -r requirements.txt
-python setup.py develop
+python3 setup.py develop
 
 echo "Setup completed."
